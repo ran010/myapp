b61d54cf4648ac5a203827b6e4e198b999555edd
ran010 <ranjanbajra@gmail.com>
2020-11-03 21:17:06 +0545

    comments

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -2,4 +2,6 @@ class Idea < ApplicationRecord
   mount_uploader :picture, PictureUploader
   validates :name, presence: true
   validates :name, uniqueness: true
+  has_many :comments
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :comments
   resources :ideas
   get "pages/info"
   root "pages#info"
